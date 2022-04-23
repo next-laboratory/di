cf6461332197eb69579ce66dd78d7b86632e9d27
chengyao <bigyao@139.com>
2022-04-23 14:06:02 +0800

    Sync code.

--- src/Exceptions/PropertyHandleException.php
+++ src/Exceptions/PropertyHandleException.php
@@ -11,9 +11,8 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Max\Di\Contracts;
+namespace Max\Di\Exceptions;
 
-interface MethodAttribute
+class PropertyHandleException extends \RuntimeException
 {
-    public function handle(\ReflectionClass $reflectionClass, \ReflectionMethod $reflectionMethod);
 }
