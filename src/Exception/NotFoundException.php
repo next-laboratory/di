669edb7d6f8e64c16dc521e74495e96576c71a3a
chengyao <64066545+topyao@users.noreply.github.com>
2022-08-11 13:59:06 +0800

    Style: cs-fix

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * @license  https://github.com/marxphp/max/blob/master/LICENSE
  */
 
-namespace Max\Di\Exceptions;
+namespace Max\Di\Exception;
 
 use Psr\Container\NotFoundExceptionInterface;
 use RuntimeException;
